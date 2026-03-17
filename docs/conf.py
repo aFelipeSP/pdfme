973f87783aac38e4f474cdf89b7dbd51407fd12b
Timothy Gabriel <timothygabrieloluwaseyi@gmail.com>
2026-03-17 18:10:04 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -30,8 +30,7 @@ author = 'Andres Felipe Sierra Parra'
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx_rtd_theme'
+    'sphinx.ext.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -53,7 +52,8 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+
+# html_static_path = ['_static']
 
 autodoc_typehints = 'none'
 autodoc_member_order = 'bysource'
